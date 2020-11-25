84d6cd9e3147dd7672ad6af849d2f25cd3d26071
Rodriguez77 <jcrodriguez0247@gmail.com>
2020-11-25 15:16:22 +0800

    FIX ADD TO QUEUE 100% all work

--- app/src/main/java/com/example/ui2/MainActivity.kt
+++ app/src/main/java/com/example/ui2/MainActivity.kt
@@ -14,20 +14,24 @@ import androidx.core.view.get
 import android.content.Intent as Intent1
 
 class MainActivity : AppCompatActivity() {
-    lateinit var songsArray: Array<String>
+//    lateinit var songsArray: Array<String>
+
+    private val songsArray = arrayOf("Like A Fire", "Spirit of God", "Leave Me Astounded",
+            "My Soul Longs for Jesus", "Endless Praise", "Only Way" , "My Reason", "Fall On Me",
+            "Rain Your Glory Down", "God Is on the Throne" , "Right Now", "I Choose You" ,
+            "This Is Our Time", "Joy", "My Heart Is Alive")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        arrayOf("Like A Fire", "Spirit of God", "Leave Me Astounded", "My Soul Longs for Jesus", "Endless Praise",
-                "Only Way" , "My Reason", "Fall On Me", "Rain Your Glory Down", "God Is on the Throne" , "Right Now", "I Choose You" ,	"This Is Our Time", "Joy", "My Heart Is Alive")
+
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songsArray)
         val songsQueueListView = findViewById<ListView>(R.id.songsQueueListView)
         songsQueueListView.adapter = adapter
 
         registerForContextMenu(songsQueueListView)
-        songsQueueListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->  }
+//        songsQueueListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->  }
     }
 
     override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
@@ -43,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         return when (item.itemId) {
             R.id.go_to_songs -> {
                 true
@@ -53,133 +58,28 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.go_to_queues -> {
-                val info = item.menuInfo as AdapterContextMenuInfo
-                albumSongs.add(songsArray[info.position])
-                startActivity(Intent1(this, SongsQueueActivity::class.java))
 
+                startActivity(Intent1(this, SongsQueueActivity::class.java))
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId) {
+
+            R.id.add_to_queue -> {
+                val info = item.menuInfo as AdapterContextMenuInfo
+                albumSongs.add(songsArray[info.position])
+                true
+            }
+            else-> super.onContextItemSelected(item)
+
         }
     }
 }
 val albumSongs = arrayListOf<String>()
-
-
-
-
-
-
-
-
-
-
-//    override fun onContextItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.add_to_queue -> {
-//                val selectedItemOrder = item!!.order
-//                val selectedItemTitle = item.title
-//
-//
-//                val info = item.menuInfo as AdapterContextMenuInfo
-//                val listPosition = info.position
-//                if(listPosition == 0) {
-//
-//                    val songname = songsArray[listPosition]
-//                    val intent = android.content.Intent(this, SongsQueueActivity::class.java)
-//                    intent.putExtra("songname", songname)
-
-
-//                    val songname = songsArray[listPosition]
-//                    intent.putExtra("songname1", songname)
-
-  //              }
-//                else if(listPosition == 1){
-//                    val songname2 = songsArray[listPosition]
-//                    intent.putExtra("songname2", songname2)
-//                }
-//                else if(listPosition == 2){
-//                    val songname3 = songsArray[listPosition]
-//                    intent.putExtra("songname3", songname3)
-//                }
-//                else if(listPosition == 3){
-//                    val songname4 = songsArray[listPosition]
-//                    intent.putExtra("songname4", songname4)
-//                }
-//                else if(listPosition == 4){
-//                    val songname5 = songsArray[listPosition]
-//                    intent.putExtra("songname5", songname5)
-//                }
-//                else if(listPosition == 5){
-//                    val songname6 = songsArray[listPosition]
-//                    intent.putExtra("songname6", songname6)
-//
-//                }
-//                else if(listPosition == 6){
-//                    val songname7 = songsArray[listPosition]
-//                    intent.putExtra("songname7", songname7)
-//                }
-//                else if(listPosition == 7){
-//                    val songname8 = songsArray[listPosition]
-//                    intent.putExtra("songname8", songname8)
-//                }
-//                else if(listPosition == 8){
-//                    val songname9 = songsArray[listPosition]
-//                    intent.putExtra("songname9", songname9)
-//                }
-//                else if(listPosition == 9){
-//                    val songname10 = songsArray[listPosition]
-//                    intent.putExtra("songname10", songname10)
-//                }
-//                else if(listPosition == 10){
-//                    val songname11 = songsArray[listPosition]
-//                    intent.putExtra("songname11", songname11)
-//                }
-//                else if(listPosition == 11){
-//                    val songname12 = songsArray[listPosition]
-//                    intent.putExtra("songname12", songname12)
-//                }
-//                else if(listPosition == 12){
-//                    val songname13 = songsArray[listPosition]
-//                    intent.putExtra("songname13", songname13)
-//                }
-//                else if(listPosition == 13){
-//                    val songname14 = songsArray[listPosition]
-//                    intent.putExtra("songname14", songname14)
-//                }
-//                else if(listPosition == 14){
-//                    val songname15 = songsArray[listPosition]
-//                    intent.putExtra("songname15", songname15)
-//                }
-
-//                true
-//            }
-//            else -> super.onContextItemSelected(item)
-//        }
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        val inflater = menuInflater
-//        inflater.inflate(R.menu.main_menu, menu)
-//        return super.onCreateOptionsMenu(menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.go_to_songs -> {
-//                true
-//            }
-//            R.id.go_to_albums -> {
-//
-//                startActivity(Intent1(this, AlbumActivity::class.java))
-//                true
-//            }
-//            R.id.go_to_queues -> {
-//                startActivity(Intent1(this, SongsQueueActivity::class.java))
-//
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-//}
--- app/src/main/java/com/example/ui2/SongsQueueActivity.kt
+++ app/src/main/java/com/example/ui2/SongsQueueActivity.kt
@@ -20,30 +20,4 @@ class SongsQueueActivity : AppCompatActivity() {
 }
 
 
-//        val intent = intent
-//        val songname = intent.getStringExtra("songname")
-
-//        val intent = intent
-//        val songs = intent.getStringExtra("songname1" )
-//        val songs2 = intent.getStringExtra("songname2")
-//        val songs3 = intent.getStringExtra("songname3")
-//        val songs4 = intent.getStringExtra("songname4")
-//        val songs5 = intent.getStringExtra("songname5")
-//        val songs6 = intent.getStringExtra("songname6")
-//        val songs7 = intent.getStringExtra("songname7")
-//        val songs8 = intent.getStringExtra("songname8")
-//        val songs9 = intent.getStringExtra("songname9")
-//        val songs10 = intent.getStringExtra("songname10")
-//        val songs11 = intent.getStringExtra("songname11")
-//        val songs12 = intent.getStringExtra("songname12")
-//        val songs13 = intent.getStringExtra("songname13")
-//        val songs14 = intent.getStringExtra("songname14")
-//        val songs15 = intent.getStringExtra("songname15")
-//
-//
-//        val songsQueueArray = arrayOf(songs, songs2, songs3, songs4, songs5, songs6, songs7, songs8, songs9, songs10, songs11, songs12, songs13, songs14, songs15)
-//        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songsQueueArray)
-//        val songsList = findViewById<ListView>(R.id.queue_songs)
-//        songsList.adapter = adapter
-
 
